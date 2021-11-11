484e6aeb0fa4e270130447e2fb218ff90b2f91dd
Charles Miele <cmiele123456@gmail.com>
2021-11-11 10:31:12 -0600

    Example 12

--- ex_03_01/ex_03_01.py
+++ ex_03_01/ex_03_01.py
@@ -2,7 +2,6 @@ hours = input('Enter Hours: ')
 rate = input('Enter Rate: ')
 float_hours = float(hours)
 float_rate = float(rate)
-# print(float_hours, float_rate)
 if float_hours > 40:
     regular_pay = 40*float_rate
     overtime_pay = (float_hours-40) * (float_rate * 1.5)
